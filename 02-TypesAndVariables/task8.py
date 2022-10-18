418ab0efa7093f11fc54f17eb5438bfd72e8835a
KamilWilczek1 <115064530+KamilWilczek1@users.noreply.github.com>
2022-10-18 17:26:22 +0200

    Add files via upload

---
+++ 02-TypesAndVariables/task8.py
@@ -0,0 +1,8 @@
+
+
+x = 7
+y = 34
+
+x, y = y, x
+
+print("After swaping values of x and y are "+ str(x), str(y))
---
+++ 02-TypesAndVariables/task9.py
@@ -0,0 +1,14 @@
+
+name = input("What is your name?: ")
+
+surname = input("What is your surname?: ")
+
+name = "Kamil"
+surname = "Wilczek"
+
+print("My name is " +name)
+print("My surname is " +surname)
+
+
+
+
